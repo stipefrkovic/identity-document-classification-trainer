e70a8a40306cb619b4076ddf43c7d006efbb0a97
Alexandru Cirlan <alexandrun.cirlan@gmail.com>
2023-05-08 19:39:29 +0200

    Now the model gets exported

--- efficientdet_training/src/run.sh
+++ efficientdet_training/src/run.sh
@@ -3,3 +3,4 @@
 python /app/src/scripts/generate_tfrecord.py -x /app/src/images/train -l /app/src/annotations/label_map.pbtxt -o /app/src/annotations/train.record
 python /app/src/scripts/generate_tfrecord.py -x /app/src/images/test -l /app/src/annotations/label_map.pbtxt -o /app/src/annotations/test.record
 python /app/src/model_main_tf2.py --model_dir=/app/src/models/my_efficientdet_d0_coco17_tpu-32 --pipeline_config_path=/app/src/models/my_efficientdet_d0_coco17_tpu-32/pipeline.config --num_train_steps=100 --checkpoint_every_n=100
+python /app/src/exporter_main_v2.py --input_type image_tensor --pipeline_config_path=/app/src/models/my_efficientdet_d0_coco17_tpu-32/pipeline.config --trained_checkpoint_dir=/app/src/models/my_efficientdet_d0_coco17_tpu-32/ --output_directory=/app/src/exported-models/saved_model
