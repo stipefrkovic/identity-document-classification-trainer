3b0bb5c289144d05370c18bcc5601dc4b958d967
Alexandru Cirlan <alexandrun.cirlan@gmail.com>
2023-05-08 19:01:35 +0200

    merge

--- README.md
+++ README.md
@@ -1,26 +1,103 @@
 # ING 2 Project - NN Trainer
 
-This python project is used to train the efficientdet model.
+This python project is used to train the machine learning model that will be used to categorise documents.
 
-## Putting the annotated images for training
+This project is executed using docker-compose.
 
-The annotated images should be put in this directory:
+## Contents
+
+* [Training Data](#training-data)
+* [Efficient Net](#efficient-net)
+* [Efficient Det](#efficient-det)
+* [Setting up](#setting-up)
+* [Running the application](#running-the-application)
+
+
+## Training Data
+
+The training data will come in a PascalVOC format, which is "a format used to store annotations for object detection datasets" [mlhive](https://mlhive.com/2022/02/read-and-write-pascal-voc-xml-annotations-in-python). Each image in the dataset will be annotated with 3 classes:
+
+* id_card
+* driving_license
+* passport
+
+The file structure will look like this:
+
+```bash
+├───Annotations
+│       passport.xml
+│       id_card.xml
+│       driving_license.xml
+|       ...
+│
+└───images
+        passport.jpg
+        id_card.jpg
+        driving_license.jpg
+        ...
 
 ```
-training_app/src/workspace/training_demo/images/train
+
+## Efficient Net
+
+EfficientNet is a machine learning model that ...
+
+This requires the training data to be provided in an image dataset format. This means that the images should be split into subdirectories based on their category. In this case it should look like the following.
+
+```bash
+├───driving_license
+│       driving_license_1.jpg
+│       driving_license_2.jpg
+│       ...
+│
+├───id_card
+│       id_card_1.jpg
+│       id_card_2.jpg
+│       ... 
+│
+└───passport
+        passport_1.jpg
+        passport_2.jpg
+        ... 
 ```
 
-## Putting the annotated images for testing
+Since this application receives a Pascal VOC format, we first need to convert the dataset. THis is done using a small script which gets the annotation classes from the xml file for each class.
 
-The annotated images should be put in this directory:
+Once the dataset conversion is complete, the model is trained. From out testing, with 50 documents it take about 10 minutes. This will increase with the number of documents.
 
+Once the model is trained, the output will be in the `model` directory.
+
+## Efficient Det
+
+The Efficient Det model is trained using a dataset in the Pascal VOC format. Therefore no conversion is necessary.
+
+## Setting up
+
+Copy the `.env` file from `.env.example`.
+
+```bash
+cp .env.example .env
 ```
-training_app/src/workspace/training_demo/images/test
+
+Create a directory called `pascalVOCInput` and put the dataset into it.
+
+```bash
+mkdir pascalVOCInput
 ```
 
-## Starting the training
+Now copy the dataset from label studio into it.
 
-To start the training you should use this command:
-```terminal
-docker-compose up --build
+## Running the application
+
+Build and run the docker compose.
+
+```bash
+docker-compose build
+docker-compose up 
 ```
+
+This will execute the applications in the following order:
+
+1. Converter
+2. Efficient Net Trainer
+3. Efficient Det Trainer
---
+++ efficientnet_training/src/efficientnet/dataset_creator.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+import tensorflow as tf
+from pathlib import Path
+import matplotlib.pyplot as plt
+from logger import logger
+
+class DatasetCreator(ABC):
+    @abstractmethod
+    def create_dataset(self):
+        pass
+
+    @abstractmethod
+    def split_dataset(self, dataset, train_split=0.7, validation_split=0.15, test_split=0.15):
+        pass
+
+
+class KerasEfficientNetDatasetCreator(DatasetCreator):
+    def create_dataset(self, dataset_path, image_size=224, batch_size=16):
+        full_dataset_path = str(Path().absolute()) + dataset_path
+        logger.debug("Dataset path: " + full_dataset_path)
+
+        dataset = tf.keras.utils.image_dataset_from_directory(full_dataset_path,
+                                                              shuffle=True,
+                                                              batch_size=batch_size,
+                                                              image_size=(image_size, image_size),
+                                                              label_mode="categorical")
+
+        # show a few images from the dataset
+        # class_names = dataset.class_names
+        # for i, (image, label) in enumerate(dataset.take(9)):
+        #     ax = plt.subplot(3, 3, i + 1)
+        #     plt.imshow(image[0].numpy().astype("uint8"))
+        #     # plt.title("{}".format(class_names(label)))
+        #     plt.axis("off")
+        # plt.show()
+
+        # augment and show a few images from the dataset
+        # data_augmentation = tf.keras.Sequential(
+        #     [
+        #         tf.keras.layers.RandomRotation(factor=0.1),
+        #         tf.keras.layers.RandomTranslation(height_factor=0.05, width_factor=0.05),
+        #         tf.keras.layers.RandomFlip(),
+        #         tf.keras.layers.RandomContrast(factor=0.1),
+        #         tf.keras.layers.RandomZoom(height_factor=(-0.1, 0), width_factor=(-0.1, 0))
+        #     ],
+        #     name="data_augmentation",
+        # )
+        # for i, (image, label) in enumerate(dataset.take(9)):
+        #     ax = plt.subplot(3, 3, i + 1)
+        #     aug_img = data_augmentation(tf.expand_dims(image[0], axis=0))
+        #     plt.imshow(aug_img[0].numpy().astype("uint8"))
+        #     plt.axis("off")
+        # plt.show()
+
+        num_classes=len(dataset.class_names)
+        logger.debug("num_classes: " + str(num_classes))
+
+        return {
+            "dataset": dataset,
+            "num_classes": num_classes
+        }
+
+    def split_dataset(self, dataset, train_split=0.7, validation_split=0.15, test_split=0.15):
+        if train_split + validation_split + test_split != 1.0:
+            raise Exception("The dataset splits do not add up to 1.")
+
+        dataset_size = dataset.cardinality().numpy()
+        logger.debug("Dataset size: " + str(dataset_size))
+
+        train_dataset_size = int(train_split * dataset_size)
+        logger.debug("Train Dataset Size: " + str(train_dataset_size))
+        validation_dataset_size = int(validation_split * dataset_size)
+        logger.debug("Validation Dataset Size: " + str(validation_dataset_size))
+        test_dataset_size = int(test_split * dataset_size)
+        logger.debug("Test Dataset Size: " + str(test_dataset_size))
+
+        train_dataset = dataset.take(train_dataset_size)
+        validation_dataset = dataset.skip(train_dataset_size).take(validation_dataset_size)
+        test_dataset = dataset.skip(train_dataset_size).skip(validation_dataset_size)
+
+        logger.debug("train_dataset.cardinality(): " + str(train_dataset.cardinality()))
+        logger.debug("validation_dataset.cardinality(): " + str(validation_dataset.cardinality()))
+        logger.debug("test_dataset.cardinality(): " + str(test_dataset.cardinality()))
+
+        train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+        validation_dataset = validation_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+        test_dataset = test_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+
+        return {
+            "train_dataset": train_dataset,
+            "validation_dataset": validation_dataset,
+            "test_dataset": test_dataset
+        }
