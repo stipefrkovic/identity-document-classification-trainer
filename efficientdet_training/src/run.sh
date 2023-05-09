9b7fc0d940f0c5108b916035f6b9b8027b0e2ee9
Alexandru Cirlan <alexandrun.cirlan@gmail.com>
2023-05-09 14:11:27 +0200

    Changed the number of steps to 5 and updated the efficientdet training.

--- efficientdet_training/src/run.sh
+++ efficientdet_training/src/run.sh
@@ -1,5 +1,5 @@
 python /app/src/scripts/partition_images.py --imageDir=/app/pascal_voc_dataset --outputDir=/app/src/images --trainSplit=0.7 --validationSplit=0.15 --testSplit=0.15 --xml
 python /app/src/scripts/generate_tfrecord.py -x /app/src/images/train -l /app/src/annotations/label_map.pbtxt -o /app/src/annotations/train.record
 python /app/src/scripts/generate_tfrecord.py -x /app/src/images/test -l /app/src/annotations/label_map.pbtxt -o /app/src/annotations/test.record
-python /app/src/model_main_tf2.py --model_dir=/app/src/models/my_efficientdet_d0_coco17_tpu-32 --pipeline_config_path=/app/src/models/my_efficientdet_d0_coco17_tpu-32/pipeline.config --num_train_steps=100 --checkpoint_every_n=100
+python /app/src/model_main_tf2.py --model_dir=/app/src/models/my_efficientdet_d0_coco17_tpu-32 --pipeline_config_path=/app/src/models/my_efficientdet_d0_coco17_tpu-32/pipeline.config --num_train_steps=5 --checkpoint_every_n=1
 python /app/src/exporter_main_v2.py --input_type image_tensor --pipeline_config_path=/app/src/models/my_efficientdet_d0_coco17_tpu-32/pipeline.config --trained_checkpoint_dir=/app/src/models/my_efficientdet_d0_coco17_tpu-32/ --output_directory=/app/src/exported-models/saved_model
