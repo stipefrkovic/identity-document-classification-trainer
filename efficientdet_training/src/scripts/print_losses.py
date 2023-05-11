d3c3050df2feb1aaef2653874fe433a18cfb33ed
Alexandru Cirlan <alexandrun.cirlan@gmail.com>
2023-05-11 21:40:32 +0200

    Solved printing evaluation and changed to save only last 10 checkpoints and save a checkpoint every 500 steps.

--- efficientdet_training/src/scripts/print_losses.py
+++ efficientdet_training/src/scripts/print_losses.py
@@ -33,6 +33,16 @@ def print_losses(train_folder_path):
                     value = struct.unpack('f', v.tensor.tensor_content)[0]
                     logger.info('Step: %s Loss: %s Tag: %s', e.step, value, v.tag)
 
+def print_all_losses(train_folder_path):
+    tfevent_files = glob.glob(os.path.join(train_folder_path, '**/*.tfevents.*'), recursive=True)
+    tfevent_files.sort(key=lambda x: os.path.getmtime(x), reverse=False)
+    for tfevent_file in tfevent_files:
+        for e in summary_iterator(tfevent_file):
+            for v in e.summary.value:
+                if v.tag == 'Loss/localization_loss' or v.tag == 'Loss/classification_loss' or v.tag == 'Loss/regularization_loss' or v.tag == 'Loss/total_loss':
+                    value = struct.unpack('f', v.tensor.tensor_content)[0]
+                    logger.info('Step: %s Loss: %s Tag: %s', e.step, value, v.tag)
+
 train_folder_path = '/app/src/models/my_efficientdet_d0_coco17_tpu-32/train/'
 eval_folder_path = '/app/src/models/my_efficientdet_d0_coco17_tpu-32/eval/'
 
@@ -40,5 +50,5 @@ logger.info('Printing losses for training.')
 print_losses(train_folder_path)
 logger.info('Finished printing losses for training.')
 logger.info('Printing losses for evaluation.')
-print_losses(eval_folder_path)
+print_all_losses(eval_folder_path)
 logger.info('Finished printing losses for evaluation.')
